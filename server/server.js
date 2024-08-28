babbeb9ca2c00adf1b5372499d4909f1d24202e8
Justin Butler <jmichaelb@mun.ca>
2024-08-28 14:03:49 -0230

    Server setup complete, also moved assets folder to be within client instead of server

--- server/server.js
+++ server/server.js
@@ -15,23 +15,6 @@ app.listen(PORT, () => {
 app.use(express.json());
 app.use(cors({ origin: "http://FRONTEND-HOST-5173?" }))
 
-
-
-// let gods = [
-//     {
-//         name: '',
-//         title: '',
-//         pantheon: ''
-//     },
-//     {
-
-//         name: '',
-//         title: '',
-//         pantheon: ''
-//     },
-// ];
-
-
 app.get("/", (_req, res) => {
     console.log("Received a GET request to /");
     res.send("Hey, here is your response");
@@ -43,14 +26,20 @@ app.get('/api/v1/gods', (_req, res) => {
     res.json(gods);
 });
 
-//   GET god by id
-app.get('/api/v1/gods/:id', (req, res) => {
-    const god = gods.find((god) => god.name === req.params.name);
+//   GET god by name
+app.get('/api/v1/gods/:name', (req, res) => {
+    const godsFile = fs.readFileSync("./data/smite2.json");
+    const gods = JSON.parse(godsFile);
+    const name =req.params.name;
+    const god = gods.find((god) => god.name.toLowerCase() === name);
+    res.json(god);
 });
 
 //   GET gods of a specific pantheon
 app.get('/api/v1/gods/pantheon/:pantheon', (req, res) => {
+    const godsFile = fs.readFileSync("./data/smite2.json");
+    const gods = JSON.parse(godsFile);
     const pantheon = req.params.pantheon;
-    const filteredGods = gods.filter((god) => (god.pantheon === pantheon));
+    const filteredGods = gods.filter((god) => (god.summary.pantheon.toLowerCase() === pantheon));
     res.json(filteredGods);
 });
